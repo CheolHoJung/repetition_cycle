d06372a425f7300937f1220c330b103e91c5d236
jcheolho <fnel159@naver.com>
2019-04-22 02:39:41 +0900

    feat: Add json parse operations to subject and subjectHistory

--- lib/subject/domain/subject.dart
+++ lib/subject/domain/subject.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:repetition_cycle/subject/domain/subject_history.dart';
 
 class Subject {
@@ -15,6 +18,46 @@ class Subject {
   Subject(this._key, this._name, this.pos);
 
   Subject.temp(this._key, this._name, this._color, this._history, this.pos);
+  Subject.temp2(this._name, this._color, this._history, this.pos);
+  Subject.fromJson(Map json) {
+    this._key = json['key'];
+    this._name = json['name'];
+    this._color = json['color'];
+    this.pos = json['pos'];
+
+    final List<SubjectHistory> history = [];
+    for (var a in json['history']) {
+      history.add(SubjectHistory.fromJson(a));
+    }
+
+    this._history = history;
+  }
+
+  Subject.fromRecord(int key, Map json) {
+    this._key = key;
+    this._name = json['name'];
+    this._color = json['color'];
+    this.pos = json['pos'];
+
+    final List<SubjectHistory> history = [];
+    for (var a in json['history']) {
+      history.add(SubjectHistory.fromJson(a));
+    }
+
+    this._history = history;
+  }
+
+  Map toJson() {
+    var map = {
+      'key': this._key,
+      'name': this._name,
+      'color': this._color,
+      'history': this._history.map((history) => history.toJson()).toList(),
+      'pos': this.pos
+    };
+
+    return map;
+  }
 
   String get days {
     if (_history.isEmpty) {
--- lib/subject/domain/subject_history.dart
+++ lib/subject/domain/subject_history.dart
@@ -5,9 +5,22 @@ class SubjectHistory {
   DateTime _datetime;
 
   SubjectHistory(this._datetime);
+  SubjectHistory.fromJson(Map json) {
+    this._datetime = DateTime.parse(json['datetime']);
+  }
 
   String ago(DateTime clock) {
-
     return timeago.format(_datetime, locale: 'ko', clock: clock);
+  }
+
+  Map toJson() {
+    return {
+      'datetime': _datetime.toString()
+    };
+  }
+
+  @override
+  String toString() {
+    return 'SubjectHistory{_datetime: $_datetime}';
   }
 }
